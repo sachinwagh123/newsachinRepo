0f0f4e92c6014c4e7b8eaa8fd3f25c80d06cd698
sachinwagh123 <sachin123wagh@gmail.com>
2023-05-02 23:36:00 +0530

    modified about

--- Hello_world.java
+++ Hello_world.java
@@ -4,7 +4,11 @@ class pakage {
 public static void main (String [] args){
 
 String str = "Hellow world";
+String s1 ="Sachin ";
+int num =10;
+int num1 =20;
 
-System.out.println(str);
+System.out.println(num + num1);
+System.out.println(str + s1);
 }
 }
